51a8bf6b08ca863fe9b038f2c86d0b1c01fc41f7
Mateusz Mikolajuk <cytius@hotmail.com>
2018-07-13 15:17:12 +0200

    added defaults to portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,4 +10,12 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ror_portfolio_items, -> { where(subtitle: 'RoR') }
+
+# defaults after creating object
+  after_initialize :set_defaults
+# ||= -> if value is nul use this
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x200"
+    self.thumb_image ||= "http://via.placeholder.com/300x150"
+  end
 end
